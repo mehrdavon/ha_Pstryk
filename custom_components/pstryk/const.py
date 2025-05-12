6b96b819005c44df6b8123e8685428f8cc3443ed
balgerion <133121849+balgerion@users.noreply.github.com>
2025-05-12 15:20:40 +0200

    Update const.py

--- custom_components/pstryk/const.py
+++ custom_components/pstryk/const.py
@@ -1,3 +1,5 @@
+"""Constants for the Pstryk Energy integration."""
+
 DOMAIN = "pstryk"
 API_URL = "https://api.pstryk.pl/integrations/"
 API_TIMEOUT = 60
@@ -8,3 +10,10 @@ SELL_ENDPOINT = "prosumer-pricing/?resolution=hour&window_start={start}&window_e
 ATTR_BUY_PRICE = "buy_price"
 ATTR_SELL_PRICE = "sell_price"
 ATTR_HOURS = "hours"
+
+# MQTT related constants
+DEFAULT_MQTT_TOPIC_BUY = "energy/forecast/buy"
+DEFAULT_MQTT_TOPIC_SELL = "energy/forecast/sell"
+CONF_MQTT_ENABLED = "mqtt_enabled"
+CONF_MQTT_TOPIC_BUY = "mqtt_topic_buy"
+CONF_MQTT_TOPIC_SELL = "mqtt_topic_sell"
